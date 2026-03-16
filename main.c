9bc61480d73c34862a44cbe1ecd9c2d0253edd10
kristinadimitrov142-23 <kristina.dimitrov.142.23@ict.edu.rs>
2026-03-16 16:57:59 +0100

    Implement addition of two user-input numbers
    Fix the calculation to display the sum of two numbers instead of their product.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+
+    printf("Unesite prvi broj: ");
+    scanf("%d", &a);
+
+    printf("Unesite drugi broj: ");
+    scanf("%d", &b);
+
+    printf("Zbir brojeva je: %d\n", a * b);
+
+    return 0;
+}
